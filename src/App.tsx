a452fadf1a1c243974ca939ecb1e7b232996905e
eduardruzga <wonderwhy.er@gmail.com>
2022-01-10 12:48:49 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React, {useCallback, useEffect, useRef} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const API_KEY = "WXAK2vDQSq6b55j9Y5QLgA";
+const API_SECRET = "vhueopJKVC9pRsnzx6uQkCFysIHe4XM7Gpgz";
+
+function App() {
+    const zoomDiv = useRef(null);
+    const handlePaste = useCallback(async (event) => {
+        if (zoomDiv.current) {
+            const pastedData = event.clipboardData.getData('Text');
+            //https://prezi.zoom.us/j/96520041636?pwd=Z3Axdkk1dURsV2c5NVU5QUdaK0lGUT09
+            if (pastedData.includes('prezi.zoom.us/j')) {
+                const meetingId = pastedData.split('/j/')[1].split('?pwd')[0];
+                const passWord = pastedData.split('?pwd=')[1];
+                const {joinZoomMeeting, monkeyPatchMediaDevices} = await import("./zoomAPI");
+                monkeyPatchMediaDevices('Prezi Camera', 'Prezi Camera');
+                const client = await joinZoomMeeting(
+                    API_KEY,
+                    API_SECRET,
+                    meetingId,
+                    'Prezi Guest',
+                    passWord,
+                    'https://prezi.com', //TODO
+                    '', // TODO
+                    zoomDiv.current,
+                );
+                console.log(client);
+            }
+        }
+    }, []);
+
+    useEffect(() => {
+        window.addEventListener("paste", handlePaste);
+        return () => {
+            window.removeEventListener("paste", handlePaste);
+        };
+    }, [handlePaste]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+            <div ref={zoomDiv}/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/zoomAPI.ts
@@ -0,0 +1,144 @@
+import crypto from 'crypto';
+import {ZoomMtg} from '@zoomus/websdk';
+import {DOMElement} from "react";
+import ZoomMtgEmbedded from "@zoomus/websdk/embedded";
+
+function addCSS(href: string) {
+    const head = document.getElementsByTagName('head')[0];
+    const s = document.createElement('link');
+    s.setAttribute('rel', 'stylesheet');
+    s.href = href;
+    head.appendChild(s);
+}
+
+addCSS('https://source.zoom.us/2.1.1/css/bootstrap.css');
+addCSS('https://source.zoom.us/2.1.1/css/react-select.css');
+ZoomMtg.setZoomJSLib('https://source.zoom.us/2.1.1/lib', '/av');
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
+// loads language files, also passes any error messages to the ui
+ZoomMtg.i18n.load('en-US');
+ZoomMtg.i18n.reload('en-US');
+
+export function monkeyPatchMediaDevices(deviceId:string, label: string) {
+    console.log('monkeyPatchMediaDevices');
+    const enumerateDevicesFn = MediaDevices.prototype.enumerateDevices;
+    const getUserMediaFn = MediaDevices.prototype.getUserMedia;
+
+    MediaDevices.prototype.enumerateDevices = async function () {
+        const res = await enumerateDevicesFn.call(navigator.mediaDevices);
+        // We could add "Virtual VHS" or "Virtual Median Filter" and map devices with filters.
+        res.push({
+            deviceId,
+            groupId: "uh",
+            kind: "videoinput",
+            label,
+            toJSON: () => {
+                return JSON.stringify({
+                    deviceId,
+                    groupId: "uh",
+                    kind: "videoinput",
+                    label,
+                });
+            }
+        });
+        return res;
+    };
+
+    MediaDevices.prototype.getUserMedia = async function (...args) {
+        const deviceId = args?.[0]?.video && typeof args?.[0]?.video !== 'boolean' && args?.[0]?.video.deviceId;
+
+        if (deviceId)  {
+            if (
+                deviceId === "virtual" ||
+                (typeof deviceId !== "string" && !Array.isArray( deviceId) && deviceId.exact === 'virtual')
+            ) {
+                // This constraints could mimick closely the request.
+                // Also, there could be a preferred webcam on the options.
+                // Right now it defaults to the predefined input.
+                return await navigator.mediaDevices.getDisplayMedia({
+                    video: true,
+                    // @ts-ignore
+                    preferCurrentTab: true
+                });
+            }
+        }
+        const res = await getUserMediaFn.call(navigator.mediaDevices, ...args);
+        return res;
+    };
+
+    console.log('VIRTUAL WEBCAM INSTALLED.')
+}
+
+export async function joinZoomMeeting(
+    apiKey: string,
+    apiSecret: string,
+    meetingNumber: string,
+    userName: string,
+    passWord: string,
+    leaveUrl: string,
+    userEmail: string,
+    element: HTMLElement,
+) {
+    const meetingConfig = {
+        apiKey,
+        meetingNumber,
+        userName,
+        passWord,
+        leaveUrl,
+        role: 0,
+        userEmail,
+        lang: 'en-US',
+        signature: ZoomMtg.generateSignature({
+            apiKey,
+            apiSecret,
+            meetingNumber,
+            role: '0',
+        }),
+        china: false,
+    };
+
+    // WebSDK Embedded init
+    var rootElement = element;
+    var zmClient = ZoomMtgEmbedded.createClient();
+
+    await zmClient.init({
+        debug: true,
+        zoomAppRoot: rootElement,
+        // assetPath: 'https://websdk.zoomdev.us/2.0.0/lib/av', //default
+        language: meetingConfig.lang,
+        customize: {
+            meetingInfo: ['topic', 'host', 'mn', 'pwd', 'telPwd', 'invite', 'participant', 'dc', 'enctype'],
+            toolbar: {
+                buttons: [
+                    {
+                        text: 'CustomizeButton',
+                        className: 'CustomizeButton',
+                        onClick: () => {
+                            console.log('click Customer Button');
+                        }
+                    }
+                ]
+            }
+        }
+    }).then((e) => {
+        console.log('success', e);
+    }).catch((e) => {
+        console.log('error', e);
+    });
+
+    // WebSDK Embedded join
+    await zmClient.join({
+        apiKey: meetingConfig.apiKey,
+        signature: meetingConfig.signature,
+        meetingNumber: meetingConfig.meetingNumber,
+        userName: meetingConfig.userName,
+        password: meetingConfig.passWord,
+        userEmail: meetingConfig.userEmail,
+    }).then((e) => {
+        console.log('success', e);
+    }).catch((e) => {
+        console.log('error', e);
+    });
+    return zmClient;
+}
